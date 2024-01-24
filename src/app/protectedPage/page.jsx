9227b9ef28d2d37c935970b7b39d0ce8a2a87357
Nazarii <nazzikk1998@gmail.com>
2024-01-24 22:59:45 +0200

    added logout button to protected page for test

--- src/app/protectedPage/page.jsx
+++ src/app/protectedPage/page.jsx
@@ -3,16 +3,27 @@
 import { logout } from "@/actions/auth";
 import withAuth from "@/hocs/withAuth";
 import { useTypedDispatch } from "@/store/store";
+import ROUTES from "@/utils/routes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ProtectedPage = () => {
   const dispatch = useTypedDispatch();
-  const handleLogout = () => {
-    dispatch(logout());
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await dispatch(logout());
+    router.push(ROUTES.auth);
   };
+
   return (
-    <div className="font-roboto bg-f8fafb flex">
-      <button onClick={handleLogout}>Logout</button>
+    <div className="font-roboto bg-f8fafb flex flex-col">
+      <button
+        onClick={handleLogout}
+        className="bg-red-500 hover:bg-red-600 text-white font-bold text-xl px-20 py-5 rounded-md shadow-md h-20 justify-center self-center"
+      >
+        Logout
+      </button>
       <div className="p-28 font-bold text-9xl text-center">
         Protected Page Content
       </div>
